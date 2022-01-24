579d8518f87b6993b8c92e618d8b60da3d2002fb
Thushara Samaraweera <thusharadasun204@gmail.com>
2022-01-24 10:37:29 +0530

    set routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
+import Login from './components/auth/Login';
+import Register from './components/auth/Register';
 import LandingPage from './components/layout/LandingPage';
 import Navbar from './components/layout/Navbar';
 
 function App() {
   return (
     <div className="App">
-      <Navbar/>
-      <LandingPage/>
+      <Router>
+        <Navbar/>
+        <Routes>
+          <Route exact path='/' element={<LandingPage/>} />
+          <Route path="/register" element={<Register/>} />
+          <Route path="/login" element={<Login/>} />
+          
+        </Routes>
+      </Router>
     </div>
   );
 }
