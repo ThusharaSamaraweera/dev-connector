4a2fbd0b233bff3707948c91de223536a05d653a
Thushara Samaraweera <thusharadasun204@gmail.com>
2022-02-08 21:57:54 +0530

    add action of adding experience, action of  adding education

--- src/store/actions/profileAction.js
+++ src/store/actions/profileAction.js
@@ -44,8 +44,70 @@ export const createProfile = (formData, navigate, edit = false) => async dispatc
   } catch (err) {
     const errors = err.response.data.errors;
     if(errors) {
-    console.log('hi')
 
+      errors.forEach(error => {
+        dispatch(setAlert(error.msg, 'danger'))
+      });
+    }
+    dispatch({
+      type: PROFILE_ERROR,
+      payload: {msg: err.response.statusTest, status: err.response.state}
+    }) 
+  }
+}
+
+// Add experience
+export const createProfile = (formData, navigate) => async dispatch => {
+  try {
+    const config = {
+      headers : {
+        'Content-Type': 'application/json'
+      }
+    }
+    const res  = await axios.put('/api/profile/experience', formData, config);
+    dispatch({
+      type: UPDATE_PROFILE,
+      payload: res.data
+    })
+
+    dispatch(setAlert('Experience added', 'success'));
+    navigate('/dashboard');
+
+  } catch (err) {
+    const errors = err.response.data.errors;
+
+    if(errors) {
+      errors.forEach(error => {
+        dispatch(setAlert(error.msg, 'danger'))
+      });
+    }
+    dispatch({
+      type: PROFILE_ERROR,
+      payload: {msg: err.response.statusTest, status: err.response.state}
+    }) 
+  }
+}
+
+export const createProfile = (formData, navigate) => async dispatch => {
+  try {
+    const config = {
+      headers : {
+        'Content-Type': 'application/json'
+      }
+    }
+    const res  = await axios.put('/api/profile/education', formData, config);
+    dispatch({
+      type: UPDATE_PROFILE,
+      payload: res.data
+    })
+
+    dispatch(setAlert('Education added', 'success'));
+    navigate('/dashboard');
+
+  } catch (err) {
+    const errors = err.response.data.errors;
+    
+    if(errors) {
       errors.forEach(error => {
         dispatch(setAlert(error.msg, 'danger'))
       });
